09524c3ff3277f888841a98e31f293ccf18c3b06
sbx_user1051 <sbx_user1051@169.254.9.9>
2022-04-21 01:58:01 +0000

    Revert "Updated VulnerableTaskHolder.java:60 - Hardened this system call to make it resistant to injected commands and commands that target sensitive files Updated VulnerableTaskHolder.java:64 - Replaced with a call that offers an upper bound on the number of characters that will be read before giving up and throwing a security exception"
    This reverts commit 9aad7032323699349c74e93283aa16c3d7d23a6c.

--- src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
+++ src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
@@ -57,11 +57,11 @@ public class VulnerableTaskHolder implements Serializable {
 				&& taskAction.length() < 22) {
 		log.info("about to execute: {}", taskAction);
 		try {
-            Process p = io.pixee.security.SystemCommand.runCommand(Runtime.getRuntime(), taskAction);
+            Process p = Runtime.getRuntime().exec(taskAction);
             BufferedReader in = new BufferedReader(
                                 new InputStreamReader(p.getInputStream()));
             String line = null;
-            while ((line = io.pixee.security.SafeIO.boundedReadLine(in, 1000000)) != null) {
+            while ((line = in.readLine()) != null) {
                 log.info(line);
             }
         } catch (IOException e) {
